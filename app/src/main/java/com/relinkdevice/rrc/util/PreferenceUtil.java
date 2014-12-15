22f3f77b41e148f033746dadeacdc86845052da1
androidcodehunter <sharif.iit.du@gmail.com>
2014-12-15 12:16:59 +0600

    #3 add PreferenceUtil.java helper class

---
+++ app/src/main/java/com/relinkdevice/rrc/util/PreferenceUtil.java
@@ -0,0 +1,117 @@
+package com.relinkdevice.rrc.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by Sharif on 12/15/2014.
+ */
+public class PreferenceUtil {
+
+    private static PreferenceUtil mPreferenceInstance;
+
+    private static SharedPreferences mSharedPreferences;
+
+    private SharedPreferences.Editor mEditor;
+
+    private static Context mContext;
+
+
+    /**
+     * Private constructor is used to initiate SingleTon instance
+     * @param  context
+     * */
+    private PreferenceUtil(Context context) {
+        this.mContext = context;
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.mContext.getApplicationContext());
+    }
+
+    /**
+     * Singleton instance
+     * @param  context
+     * */
+    public static synchronized PreferenceUtil getInstance(Context context) {
+        if (mPreferenceInstance == null) {
+            mPreferenceInstance = new PreferenceUtil(context);
+        }
+        return mPreferenceInstance;
+    }
+
+    /**
+     * Get preference value using key
+     * @param  key
+     * @param defaultValue
+     * */
+    public String getString(String key, String defaultValue) {
+        return mSharedPreferences.getString(key, defaultValue);
+    }
+
+    /**
+     * Set preference string
+     * @param  key
+     * @param value
+     * */
+    public void setString(String key, String value) {
+        getmEditor().putString(key, value).commit();
+    }
+
+    /**
+     * Get boolean preference if the value is available otherwise it will return false
+     * @param  key
+     * */
+    public boolean getBoolean(String key) {
+        return mSharedPreferences.getBoolean(key, false);
+    }
+
+    /**
+     * Get boolean preference
+     * @param  key
+     * @param defaultValue
+     * */
+    public boolean getBoolean(String key, boolean defaultValue) {
+        return mSharedPreferences.getBoolean(key, defaultValue);
+    }
+
+    /**
+     * Set boolean preference
+     * @param  key
+     * @param value
+     * */
+    public void setBoolean(String key, boolean value) {
+        getmEditor().putBoolean(key, value).commit();
+    }
+
+    /**
+     * Set int preference
+     * @param  key
+     * @param value
+     * */
+    public void setInt(String key, int value) {
+        getmEditor().putInt(key, value).commit();
+    }
+
+    /**
+     * Get int preference
+     * @param  key
+     * @param defaultValue
+     * */
+    public int getInt(String key, int defaultValue) {
+        return mSharedPreferences.getInt(key, defaultValue);
+    }
+
+    /**
+     * Get int preference where default value is zero
+     * @param  key
+     * */
+    public int getInt(String key) {
+        return mSharedPreferences.getInt(key, 0);
+    }
+
+    /**
+     * Editor to write into SharedPreference file
+     * */
+    private SharedPreferences.Editor getmEditor(){
+        return mSharedPreferences.edit();
+    }
+}
