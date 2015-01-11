14844a498883b7687e9016a1fe1184b11baa60fa
Trajche Nakov <trajce.nakov@gmail.com>
2015-01-11 19:25:25 +0100

    Small clean-up

--- remotecontrol-demo/src/main/java/com/relinkdevice/rrc/activities/MainActivity.java
+++ remotecontrol-demo/src/main/java/com/relinkdevice/rrc/activities/MainActivity.java
@@ -35,8 +35,7 @@ public class MainActivity extends ActionBarActivity {
     public static final String DEVICE_NAME = "device_name";
     public static final String TOAST = "toast";
     // Intent request codes
-    private static final int REQUEST_CONNECT_DEVICE = 1;
-    private static final int REQUEST_ENABLE_BT = 2;
+    private static final int REQUEST_ENABLE_BT = 1;
 
     private Spinner mVolumeTypes, mVolumeLevels;
 
